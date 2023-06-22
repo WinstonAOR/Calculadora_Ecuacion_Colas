52eb39563f7779bd3b2309772a1008373fec8a05
WinstonAOR <winkyortegar@hotmail.com>
2023-06-22 14:41:32 -0600

    Creating MM1 Infinito

--- js/functions.js
+++ js/functions.js
@@ -35,13 +35,13 @@ function calcMM1(){
     resultPnk = (Math.pow(pnk1, pnk2))*100;
     console.log(resultPnk);
     document.getElementById("tbMM1").style.display="block";
-    document.getElementById("mm1_l").value=resultL;
-    document.getElementById("mm1_lq").value=resultLq;
-    document.getElementById("mm1_ro").value=resultRo + "%";
-    document.getElementById("mm1_pnk").value=resultPnk + "%";
-    document.getElementById("mm1_p0").value=resultP0 + "%";
-    document.getElementById("mm1_w").value=resultW;
-    document.getElementById("mm1_wq").value=resultWq;
+    document.getElementById("mm1_l").value=resultL.toFixed(4);
+    document.getElementById("mm1_lq").value=resultLq.toFixed(4);
+    document.getElementById("mm1_ro").value=resultRo.toFixed(4) + "%";
+    document.getElementById("mm1_pnk").value=resultPnk.toFixed(4) + "%";
+    document.getElementById("mm1_p0").value=resultP0.toFixed(4) + "%";
+    document.getElementById("mm1_w").value=resultW.toFixed(4);
+    document.getElementById("mm1_wq").value=resultWq.toFixed(4);
     
     
 }
@@ -85,12 +85,12 @@ function calcMMn(){
     resultRo = (lambda/(m*mu));
     console.log(resultRo);
     document.getElementById("tbMMn").style.display="block";
-    document.getElementById("mmn_l").value=resultL;
-    document.getElementById("mmn_lq").value=resultLq;
-    document.getElementById("mmn_ro").value=resultRo;
-    document.getElementById("mmn_p0").value=resultP0;
-    document.getElementById("mmn_w").value=resultW;
-    document.getElementById("mmn_wq").value=resultWq;
+    document.getElementById("mmn_l").value=resultL.toFixed(4);
+    document.getElementById("mmn_lq").value=resultLq.toFixed(4);
+    document.getElementById("mmn_ro").value=resultRo.toFixed(4);
+    document.getElementById("mmn_p0").value=resultP0.toFixed(4);
+    document.getElementById("mmn_w").value=resultW.toFixed(4);
+    document.getElementById("mmn_wq").value=resultWq.toFixed(4);
    
     
     
@@ -129,10 +129,10 @@ function calcMD1(){
     
     
     document.getElementById("tbMD1").style.display="block";
-    document.getElementById("md1_l").value=resultL;
-    document.getElementById("md1_lq").value=resultLq;
-    document.getElementById("md1_w").value=resultW;
-    document.getElementById("md1_wq").value=resultWq;   
+    document.getElementById("md1_l").value=resultL.toFixed(4);
+    document.getElementById("md1_lq").value=resultLq.toFixed(4);
+    document.getElementById("md1_w").value=resultW.toFixed(4);
+    document.getElementById("md1_wq").value=resultWq.toFixed(4);   
     
 }
 
@@ -146,5 +146,51 @@ function cleanMD1(){
     document.getElementById("md1_p0").value="";
     document.getElementById("md1_w").value="";
     document.getElementById("md1_wq").value="";
+    
+}
+
+/**Sumatoria */
+
+function SumatoriaMM1I(g, division){
+    var  result = 0;
+    for (j=0; j<=g; j++) {
+        result += ((factorial(g)/((factorial(g-j))))*(Math.pow(division,j)));
+    }
+    return result;
+}
+
+/** MM1I */
+
+function calcMM1I(){
+    var resultL, resultP0, resultLq, resultWq = 0;
+    var mu = document.getElementById("mm1i_1").value;
+    var lambda = document.getElementById("mm1i_2").value;
+    var n = document.getElementById("mm1i_3").value;
+    resultP0 = (1/SumatoriaMM1I(n,lambda/mu));
+    resultLq = (n-((parseFloat(lambda)+parseFloat(mu))/lambda)*(1-resultP0));
+    console.log(resultLq);
+    resultL = (resultLq+(1-resultP0));
+    console.log(resultL);
+    resultWq = (resultLq/((n-resultL)*lambda));
+    console.log(resultWq);
+    
+    
+    document.getElementById("tbMM1I").style.display="block";
+    document.getElementById("mm1i_l").value=resultL.toFixed(4);
+    document.getElementById("mm1i_lq").value=resultLq.toFixed(4);
+    document.getElementById("mm1i_p0").value=resultP0.toFixed(4);
+    document.getElementById("mm1i_wq").value=resultWq.toFixed(4);   
+    
+}
+
+function cleanMM1I(){
+    document.getElementById("tbMM1I").style.display="none";
+    document.getElementById("mm1i_1").value="";
+    document.getElementById("mm1i_2").value="";
+    document.getElementById("mm1i_3").value="";
+    document.getElementById("mm1i_l").value="";
+    document.getElementById("mm1i_lq").value="";
+    document.getElementById("mm1i_p0").value="";
+    document.getElementById("mm1i_wq").value="";
     
 }
